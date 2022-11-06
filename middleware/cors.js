9625fdfbf8d8ca1eb119b5acd6475f565b788ace
Mihajlo Kucak <kucakmihajloskola@gmail.com>
2022-11-06 20:11:30 +0100

    please

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,9 +1,6 @@
 function corsMiddleware(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 }
 
